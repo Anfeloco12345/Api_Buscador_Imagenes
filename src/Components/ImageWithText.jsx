c1b4d3b2598ba9ade9ab10f8186a403911ec1f71
Andres Lozano <anfelipelc3@gmail.com>
2023-11-08 01:18:12 -0500

    Depurador

--- src/Components/ImageWithText.jsx
+++
@@ -1,9 +0,0 @@
-function ImageWithText(props) {
-    return (
-    <div>
-        <img src={props.image} alt="Una imagen" width="200" height="400"/>
-        <p>{props.text}</p>
-    </div>
-    );
-}
-export default ImageWithText;
