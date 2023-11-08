d2b57899c481cc614e58354300fdb85a7da28ac1
Andres Lozano <anfelipelc3@gmail.com>
2023-11-08 01:01:50 -0500

    buscador Imagenes

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [valor, setValor] = useState('');
+  const [resultados, setResultados] = useState([]);
+
+  const buscarResultados = async () => {
+    const API_KEY = '5Ozvt6r4qz7j2tvAOZCl5N2QYar6ja36q85zJyqebok'
+    const URL = `https://api.unsplash.com/search/photos/?client_id=${API_KEY}&query=${valor}&per_page=20`;
+
+    const response = await fetch(URL);
+    const data = await response.json();
+    setResultados(data.results)
+    console.log(data);
+  }
+
+  return (
+    <div className="App">
+      <div className='searchBox'>
+        <input className='searchBoxInput' placeholder='Buscar imagenes en ingles' onChange={e => setValor(e.target.value)} />
+        <button onClick={() => buscarResultados()} className='searchBoxBtn'>BUSCAR</button>
+      </div>
+
+      <div className='mainContent'>
+        <div className='mainContentGrid'>
+          {
+            resultados.map((elemento, indice) => {
+              return (
+                <img key={indice} src={elemento.urls.regular}/>
+              )
+            })
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
